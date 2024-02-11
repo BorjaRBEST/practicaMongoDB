9face5791f35087ef740daa92870a60c3845aa37
pcorriente <pcorrientetorres@gmail.com>
2024-02-11 23:03:39 +0100

    Se ha creado un método en la clase Metodos para crear nuevas colecciones con su correspondiente llamada en la clase Menu (Op. 12) y se ha comentado el código al completo. Saludaciones.

--- Conexion.py
+++ Conexion.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
 
 class Conexion:
+    # Constructor de la clase, recibe la URI de conexión como parámetro
     def __init__(self, uri):
+        # Creamos una instancia de MongoClient utilizando la URI proporcionada
         self.client = MongoClient(uri)
 
+    # Método para obtener el cliente de conexión a la base de datos qie devuelve el cliente de conexión
     def get_client(self):
         return self.client
--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import Menu
 
 class Main:
     if __name__ == "__main__":
+        # URI de conexión a la base de datos MongoDB
         uri = "mongodb+srv://borjarodriguez203:medac123@clustermedac.pzeoa6p.mongodb.net/?retryWrites=true&w=majority"
+        # Creamos una instancia de la clase Conexion, pasando la URI como argumento
         conexion = Conexion.Conexion(uri)
+        # Creamos una instancia de la clase Metodos, pasando la instancia de Conexion como argumento
         metodos = Metodos.Metodos(conexion)
+        # Creamos una instancia de la clase Menu, pasando la instancia de Metodos como argumento
         menu = Menu.Menu(metodos)
+        # Llamamos al método menu_principal() para iniciar el programa
         menu.menu_principal()
