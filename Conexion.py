eba84f2ced8452bc23e4492cad88c2a25f59f118
Borja Rodriguez <borjarodriguez203@gmail.com>
2024-02-12 05:28:47 +0100

    Commit agregación comentarios en conexión

--- Conexion.py
+++ Conexion.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 
+# Clase Conexion donde ubicamos pymongo para llamarlo en el Main
 class Conexion:
     def __init__(self, uri):
         self.client = MongoClient(uri)
