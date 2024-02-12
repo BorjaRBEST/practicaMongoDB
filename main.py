7f7b9e5596152dafdb6ad529dacd5afb72ebedfb
Borja Rodriguez <borjarodriguez203@gmail.com>
2024-02-12 06:37:11 +0100

    Commit final proyecto (pequeño cambio en el main para manejar excepcción al detener programa)

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import Metodos
 import Menu
 
 class Main:
-    if __name__ == "__main__":
-        # URI de conexión a la base de datos MongoDB
-        uri = "mongodb+srv://borjarodriguez203:medac123@clustermedac.pzeoa6p.mongodb.net/?retryWrites=true&w=majority"
-        # Creamos una instancia de la clase Conexion, pasando la URI como argumento
-        conexion = Conexion.Conexion(uri)
-        # Creamos una instancia de la clase Metodos, pasando la instancia de Conexion como argumento
-        metodos = Metodos.Metodos(conexion)
-        # Creamos una instancia de la clase Menu, pasando la instancia de Metodos como argumento
-        menu = Menu.Menu(metodos)
-        # Llamamos al método menu_principal() para iniciar el programa
-        menu.menu_principal()
+    try:
+        if __name__ == "__main__":
+            # URI de conexión a la base de datos MongoDB
+            uri = "mongodb+srv://borjarodriguez203:medac123@clustermedac.pzeoa6p.mongodb.net/?retryWrites=true&w=majority"
+            # Creamos una instancia de la clase Conexion, pasando la URI como argumento
+            conexion = Conexion.Conexion(uri)
+            # Creamos una instancia de la clase Metodos, pasando la instancia de Conexion como argumento
+            metodos = Metodos.Metodos(conexion)
+            # Creamos una instancia de la clase Menu, pasando la instancia de Metodos como argumento
+            menu = Menu.Menu(metodos)
+            # Llamamos al método menu_principal() para iniciar el programa
+            menu.menu_principal()
+    except KeyboardInterrupt:
+        print("Programa detenido por el usuario")
